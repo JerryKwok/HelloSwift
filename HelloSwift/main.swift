4aa6b9ca133a936093c0fb2a1a0878f5a1fa9594
郭大侠 <jerry_kwok@foxmail.com>
2017-02-09 11:47:17 +0800

    Initial Commit

---
+++ HelloSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloSwift
+//
+//  Created by popo-fish on 2017/02/09.
+//  Copyright © 2017年 popo-fish. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
