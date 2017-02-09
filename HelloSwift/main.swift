813fcce9a5e76b0f96a4efdbf2741550271c8fd4
郭大侠 <jerry_kwok@foxmail.com>
2017-02-09 17:27:06 +0800

    pdf教程看到77页

--- HelloSwift/main.swift
+++ HelloSwift/main.swift
@@ -1,12 +1,80 @@
-//
-//  main.swift
-//  HelloSwift
-//
-//  Created by popo-fish on 2017/02/09.
-//  Copyright © 2017年 popo-fish. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+/*
+enum ErrorType:Error
+{
+    case ErrorType1
+    case ErrorType2
+}
+
+func doSomething(value:Int) throws
+{
+    if value == 1
+    {
+        throw ErrorType.ErrorType1
+    }
+    if value == 2
+    {
+        throw ErrorType.ErrorType2
+    }
+}
+
+do
+{
+    try doSomething(value: 1)
+}
+catch ErrorType.ErrorType1
+{
+    print("Error1")
+}
+catch ErrorType.ErrorType2
+{
+    print("Error2")
+}
+
+
+do
+{
+    try doSomething(value: 2)
+}
+catch
+{
+    switch(error)
+    {
+    case ErrorType.ErrorType1:
+        print("Error1")
+    case ErrorType.ErrorType2:
+        print("Error2")
+    default:
+        print("Unknown Error")
+    }
+}
+
+*/
+
+
+let names = ["a","b","c","d"]
+for index in 0..<names.count
+{
+    print(names[index])
+}
+
+let str = ""
+if str.isEmpty
+{
+    print("str is empty")
+}
+
+
+var str1 = "aa"
+let c:Character = "x"
+str1.append(c)
+let str2 = "\\\\"
+
+
+
+
+
+
+
 
